df69d8bfa17580d6711ff1781cbba64e714efe9b
kimminsun <minsun2767@naver.com>
2017-05-24 23:19:34 +0900

    #10 python study

---
+++ ch02/if01.py
@@ -0,0 +1,35 @@
+#if 문
+
+money=1
+if money:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+
+#조건문이란 무엇인가
+x=3
+y=2
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+
+money=2000
+if money>=3000:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+
+
+#add or not
+
+money=2000
+card=1
+if money>=3000 or card:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+
+
