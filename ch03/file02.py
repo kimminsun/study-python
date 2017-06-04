8851ad86c2fe13a7f4ab39a16cefc05e1dc5ce7e
kimminsun <minsun2767@naver.com>
2017-06-04 17:49:46 +0900

    #19 python study

--- ch03/file02.py
+++ ch03/file02.py
@@ -4,3 +4,13 @@ for i in range(11,20):
     data="%d번째 줄 입니다.\n" %i
     f.write(data)
 f.close()
+
+#with문과 함께 사용하기
+f=open("foo.txt",'w')
+f.write("Life is too short, you need python")
+f.close()
+
+with open("foo.txt","w") as f:
+    f.write("Life is too short, you need python")
+
+    
