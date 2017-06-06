5d3eae85ca1dfcbc1faf06a462a10e318e1f6228
kimminsun <minsun2767@naver.com>
2017-06-06 20:36:39 +0900

    #21 python study

---
+++ ch04/class01.py
@@ -0,0 +1,29 @@
+result=0
+
+def adder(num):
+    global result
+    result+=num
+    return result
+
+print(adder(3))
+print(adder(4))
+
+
+
+
+#클래스 함수
+
+class Service:
+    secret="영구는 배꼽이 두 개다"
+    def sum(self,a,b):
+        result=a+b
+        print("%s+%s=%s입니다,"%(a,b,result))
+
+
+pey=Service()
+pey.sum(1,1)
+
+
+
+
+
