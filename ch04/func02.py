548b88688e42b5e86c7879e62ad00f6d02a8587c
kimminsun <minsun2767@naver.com>
2017-06-11 17:42:29 +0900

    #30 python study

---
+++ ch04/func02.py
@@ -0,0 +1,9 @@
+#enumerate 열거하다
+for i,name in enumerate(['body','foo','bar']):
+    print(i,name)
+
+#eval 실행가능한 문자열을 입력으로 받아 문자열을 실행한 결과값을 리턴
+print(eval('1+2'))
+print(eval("('hi'+'a')"))
+print(eval('divmod(4,3)'))
+
