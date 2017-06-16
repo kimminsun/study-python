92e121f7af81ab4ad84201f40b6250c2a6fef6c3
kimminsun <minsun2767@naver.com>
2017-06-16 21:20:33 +0900

    #33 python study

--- ch04/func02.py
+++ ch04/func02.py
@@ -26,3 +26,30 @@ def positive(x):
 
 print(list(filter(positive,[1,-3,2,0,-5,6])))
 
+
+
+#hex 정수값을 입력받아 16진수로 변환하여 리턴하는 함수
+print(hex(234))
+print(hex(3))
+
+
+#id 객체를 입력받아 객체의 고유 주소값을 리턴하는 함수
+a=3
+print(id(3))
+print(id(a))
+b=a
+print(id(b))
+
+print(id(4))
+
+
+#input 사용자 입력을 받는 함수이다
+a=input()
+print(a)
+
+b=input("Enter : ")
+print(b)
+
+
+
+
