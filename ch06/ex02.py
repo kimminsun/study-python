f9412ffd4183405986039a96bdf225d6dca22ff7
kimminsun <minsun2767@naver.com>
2017-06-29 23:26:57 +0900

    #52 python study

--- ch06/ex02.py
+++ ch06/ex02.py
@@ -1,26 +1,9 @@
-data="""
-park 800905-1049118
-kim 700905-1059119
-"""
-
-result=[]
-for line in data.split("\n"):
-    word_result=[]
-    for word in line.split(" "):
-        if len(word)==14 and word[:6].isdigit() and word[7:].isdigit():
-            word=word[:6]+"-"+"******"
-        word_result.append(word)
-    result.append(" ".join(word_result))
-print("\n".join(result))
-
-
-#정규식 표현방법
-
 import re
-data="""
-park 800905-1049118
-kim 700905-1059119
-"""
+p=re.compile('[a-z]+')
 
-pat=re.compile("(\d{6})[-]\d{7}")
-print(pat.sub("\g<1>-*******",data))
+m=p.match("python")
+print(m)
+
+m=p.match("3 python")
+print(m)
+
